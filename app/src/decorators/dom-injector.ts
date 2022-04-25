17fb99261cd426527c6c00bcdeebe904c79dc131
LeandroMenoci <menoci.leandro@gmail.com>
2022-04-25 11:01:13 -0300

    Evitando codigo duplicado / Decorator de propriedade / Object.defineProperty

---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,18 @@
+export function domInjector(selector: string) {
+  return function (target: any, propertyKey: string) {
+
+    let elemento: HTMLElement | null = null
+    const getter = function () {
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(selector)
+        console.log(`buscando elemento do DOM com o seletor ${selector} para injetar em ${propertyKey} `)
+      }
+
+      return elemento
+    }
+
+    Object.defineProperty(target, propertyKey, {
+      get: getter
+    })
+  }
+}
